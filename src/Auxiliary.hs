23d97d21956d523af7243b933a233dca69a35e5a
kurnevsky <kurnevsky@gmail.com>
2014-03-22 22:47:33 +0300

    Some refactoring.

--- src/Auxiliary.hs
+++ src/Auxiliary.hs
@@ -5,32 +5,30 @@ import Data.Char (isSpace)
 
 trim :: String -> String
 trim = f . f
-        where f = reverse . dropWhile isSpace
+  where f = reverse . dropWhile isSpace
 
 removeNearSame :: Eq a => [a] -> [a]
 removeNearSame [] = []
 removeNearSame [a] = [a]
-removeNearSame (h:t) = if h == head t
-                       then removeNearSame t
-                       else h : removeNearSame t
+removeNearSame (h : t) | h == head t = removeNearSame t
+                       | otherwise   = h : removeNearSame t
 
 count :: (a -> Bool) -> [a] -> Int
 count _ [] = 0
-count p (x:xs) | p x       = 1 + count p xs
-               | otherwise = count p xs
+count p (x : xs) | p x       = 1 + count p xs
+                 | otherwise = count p xs
 
 leftShift1 :: [a] -> [a]
 leftShift1 list = tail list ++ [head list]
 
 replaceSingle :: (a -> Bool) -> (a -> a) -> [a] -> [a]
 replaceSingle _ _ [] = error "replaceSingle: not found."
-replaceSingle f g (h:t) | f h = g h : t
-                        | otherwise = h : replaceSingle f g t
+replaceSingle f g (h : t) | f h       = g h : t
+                          | otherwise = h : replaceSingle f g t
 
 padRight :: a -> Int -> [a] -> [a]
-padRight c n s = if length s >= n
-                 then s
-                 else s ++ replicate (n - length s) c
+padRight c n s | length s >= n = s
+               | otherwise     = s ++ replicate (n - length s) c
 
 toTreeInv :: [a] -> Tree a
 toTreeInv [] = error "toTreeInv: empty list."
